5df9f7b106a9aae18b4db841229a50fb3ac08409
Joel Davalos <jsdavalos@gmail.com>
2024-10-23 14:48:50 -0600

    Added show route.

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PostersController < ApplicationController
   def index
     posters = Poster.all
-    render json: PosterSerializer.format_posters(posters)
+    render json: posters #PosterSerializer.format_posters(posters)
   end
 end

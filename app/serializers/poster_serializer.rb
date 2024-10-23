8ecd430ca302cf50a80a03f3f84284208bf5aeaf
James-Cochran <jameslikesfruit@gmail.com>
2024-10-23 10:04:44 -0400

    refactored endpoints test file, moved test posters to a before block.  Created tests for the update/patch endpoint.

--- app/serializers/poster_serializer.rb
+++ app/serializers/poster_serializer.rb
@@ -1,22 +1,20 @@
-class PosterSerializer
-  def self.format_posters(posters)
-    posters.map do |poster|
-      {
-        data: [
-         {
-           id: poster.id,
-           type: "poster",
-           attributes: {
-             name: poster.name,
-             description: poster.description,
-             price: poster.price,
-             year: poster.year,
-             vintage: poster.vintage,
-             img_url: poster.img_url
-           }
-         } 
-        ]
-      }
+class PosterSerializer #< ActiveModel::Serializer
+  # attributes :id, :name, :description, :price, :year, :vintage, :img_url
+
+    def self.format_posters(posters)
+      posters.map do |poster|
+          {
+            id: poster.id,
+            type: "poster",
+            attributes: {
+              name: poster.name,
+              description: poster.description,
+              price: poster.price,
+              year: poster.year,
+              vintage: poster.vintage,
+              img_url: poster.img_url
+            }
+          } 
+        end
     end
-  end
 end

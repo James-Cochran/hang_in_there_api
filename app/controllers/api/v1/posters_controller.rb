645b0d29a3a4082542453ae1929ea68265fab1c7
James-Cochran <jameslikesfruit@gmail.com>
2024-10-23 15:32:41 -0400

    added update route, added update action to posters controller, added poster params to posters controller.

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::PostersController < ApplicationController
     posters = Poster.all
     render json: PosterSerializer.format_posters(posters)
   end
+
+  def update
+    updated_poster = Poster.update(params[:id], poster_params)
+    render json: PosterSerializer.format_posters(updated_poster)
+  end
+
+  private
+
+  def poster_params
+    params.require(:poster).permit(:name, :description, :price, :year, :vintage, :img_url)
+  end
 end
